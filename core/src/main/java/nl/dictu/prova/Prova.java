6c6846108817fdcac9d9b0ab604a5bd5e67ef6ea
Sjoerd Boerhout <sjoerdboerhout@gmail.com>
2016-04-03 20:44:12 +0200

    PROVA-13: Configureerbare logging naar bestand
    Note: src/rescources must be in the class path for log4j2.xml to be found

--- core/src/main/java/nl/dictu/prova/Prova.java
+++ core/src/main/java/nl/dictu/prova/Prova.java
@@ -1,13 +1,133 @@
 package nl.dictu.prova;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+
+import nl.dictu.prova.logging.LogLevel;
+
 /*
  * Hello world!
  *
  */
 public class Prova
 {
+  private static LogLevel logLevel = LogLevel.WARNING;
+  
+  final static Logger LOGGER = LogManager.getLogger();
+
   public static void main(String[] args)
   {
-    System.out.println("Hello World!");
+    Prova prova = new Prova(args);
   }
+
+  private static String ReturnString(String text)
+  {
+    // For testing the Lambda support of Log4J2
+    // This is a very complicated function!
+    return text;
+  }
+
+  private static String setDebugLevel(String logger, String name)
+  {
+    try
+    {
+      logLevel = LogLevel.lookup(name);
+      
+      System.setProperty("prova.log.level", logLevel.name());
+      LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+      ctx.reconfigure();
+      
+      System.out.println("Config updated. Level: " + ctx.getLogger(LogManager.ROOT_LOGGER_NAME).getLevel());
+      
+      return ctx.getLogger(LogManager.ROOT_LOGGER_NAME).getLevel().name();
+    }
+    catch(Exception e)
+    {
+      // TODO Implement error handling
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+    }
+    
+    return "";
+  }
+  
+  
+  private static void updateLogfile(String logFile)
+  {     
+    System.setProperty("prova.log.filename", logFile);
+      
+      LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+      ctx.reconfigure();
+    }
+    
+ 
+  private static void updateLogPattern(String newPattern)
+  {
+    System.setProperty("prova.log.pattern.console", newPattern);
+    System.setProperty("prova.log.pattern.file", newPattern);
+    
+    LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+    ctx.reconfigure();
+  }
+
+  /*
+   * TODO Add Javadoc
+   */
+  public Prova(String[] args)
+  {
+    try
+    {
+      System.out.println( "Hello World, I'm Prova!" );
+          
+      setDebugLevel(Prova.class.getName(), args.length > 0 ? args[0] : "error");
+      
+      Logger LOGGER = LogManager.getLogger();
+          
+      LOGGER.fatal("Fatale fout - {}", () -> ReturnString("Blaat"));
+      LOGGER.error("Niet fatale Error");
+      LOGGER.warn("Oeps...");
+      LOGGER.info("InfoBericht");
+      LOGGER.debug("DebugMessage");
+      LOGGER.trace("Trace datadump");
+  
+      System.out.println("----------");
+  
+      try 
+      {
+        Thread.sleep(2000);
+      } 
+      catch(InterruptedException ex) 
+      {
+        Thread.currentThread().interrupt();
+      }
+  
+      setDebugLevel(Prova.class.getName(), args.length > 1 ? args[1] : "trace");
+      updateLogPattern("%d{yyyy-MM-dd HH:mm:ss:SSS} [%c:%t:%L] %-5p - %msg%n");
+      
+      LOGGER.fatal("Fatale fout - {}", () -> ReturnString("Schaap"));
+      LOGGER.error("Niet fatale Error");
+      LOGGER.warn("Oeps...");
+      LOGGER.info("InfoBericht");
+      LOGGER.debug("DebugMessage");
+      LOGGER.trace("Trace datadump");
+      
+      System.out.println("----------");
+      updateLogfile("ProvaX");
+
+      LOGGER.fatal("Fatale fout - {}", () -> ReturnString("Geit"));
+      LOGGER.error("Niet fatale Error");
+      LOGGER.warn("Oeps...");
+      LOGGER.info("InfoBericht");
+      LOGGER.debug("DebugMessage");
+      LOGGER.trace("Trace datadump");
+    }
+    catch(Exception e)
+    {
+      // TODO Implement error handling
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+    }
+  }
+    
 }
---
+++ core/src/test/java/nl/dictu/prova/logging/LogLevelTest.java
@@ -0,0 +1,108 @@
+package nl.dictu.prova.logging;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Sjoerd Boerhout
+ * 
+ * Issue ID: PROVA-13
+ *
+ */
+public class LogLevelTest
+{  
+  /*
+  OFF(0),
+  FATAL(1),
+  ERROR(2),
+  WARNING(3),
+  INFO(4),
+  DEBUG(5),
+  TRACE(6),
+  ALL(7);
+  */
+  
+  @Test
+  public void checkNumberOfEnums()
+  {
+    assertTrue(LogLevel.values().length == 8);
+  }
+  
+  @Test
+  public void checkValuesOfEnums()
+  {
+    assertTrue(LogLevel.OFF.getValue()      == 0);
+    assertTrue(LogLevel.FATAL.getValue()    == 1);
+    assertTrue(LogLevel.ERROR.getValue()    == 2);
+    assertTrue(LogLevel.WARNING.getValue()  == 3);
+    assertTrue(LogLevel.INFO.getValue()     == 4);
+    assertTrue(LogLevel.DEBUG.getValue()    == 5);
+    assertTrue(LogLevel.TRACE.getValue()    == 6);
+    assertTrue(LogLevel.ALL.getValue()      == 7);
+  }
+  
+  @Test
+  public void checkOff()
+  {    
+    assertTrue( LogLevel.lookup("off").name().equals("OFF") );
+    assertTrue( LogLevel.lookup("OFF").name().equals("OFF") );
+    assertTrue( LogLevel.lookup("OFF").toString().equals("OFF") );
+  }
+
+  @Test
+  public void checkFatal()
+  {
+    assertTrue( LogLevel.lookup("fatal").name().equals("FATAL") );
+    assertTrue( LogLevel.lookup("FATAL").name().equals("FATAL") );
+    assertTrue( LogLevel.lookup("FATAL").toString().equals("FATAL") );
+  }
+
+  @Test
+  public void checkError()
+  {    
+    assertTrue( LogLevel.lookup("error").name().equals("ERROR") );
+    assertTrue( LogLevel.lookup("ERROR").name().equals("ERROR") );
+    assertTrue( LogLevel.lookup("ERROR").toString().equals("ERROR") );
+  }
+
+  @Test
+  public void checkWarning()
+  {    
+    assertTrue( LogLevel.lookup("warning").name().equals("WARNING") );
+    assertTrue( LogLevel.lookup("WARNING").name().equals("WARNING") );
+    assertTrue( LogLevel.lookup("WARNING").toString().equals("WARNING") );
+  }
+
+  @Test
+  public void checkInfo()
+  {    
+    assertTrue( LogLevel.lookup("info").name().equals("INFO") );
+    assertTrue( LogLevel.lookup("INFO").name().equals("INFO") );
+    assertTrue( LogLevel.lookup("INFO").toString().equals("INFO") );
+  }
+
+  @Test
+  public void checkDebug()
+  {    
+    assertTrue( LogLevel.lookup("debug").name().equals("DEBUG") );
+    assertTrue( LogLevel.lookup("DEBUG").name().equals("DEBUG") );
+    assertTrue( LogLevel.lookup("DEBUG").toString().equals("DEBUG") );
+  }
+
+  @Test
+  public void checkTrace()
+  {    
+    assertTrue( LogLevel.lookup("trace").name().equals("TRACE") );
+    assertTrue( LogLevel.lookup("TRACE").name().equals("TRACE") );
+    assertTrue( LogLevel.lookup("TRACE").toString().equals("TRACE") );
+  }
+
+  @Test
+  public void checkAll()
+  {    
+    assertTrue( LogLevel.lookup("all").name().equals("ALL") );
+    assertTrue( LogLevel.lookup("ALL").name().equals("ALL") );
+    assertTrue( LogLevel.lookup("ALL").toString().equals("ALL") );
+  }
+}
